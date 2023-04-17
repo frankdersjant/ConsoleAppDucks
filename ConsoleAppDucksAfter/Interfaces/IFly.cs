a3a79f4356c10cab3125f96a9e1168e6ec3998c2
Frank Dersjant <Frank.Dersjant@inholland.nl>
2023-04-17 11:45:59 +0200

    further cleanup

--- ConsoleAppDucksAfter/Interfaces/IFly.cs
+++ ConsoleAppDucksAfter/Interfaces/IFly.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleAppDucksAfter.Interfaces
+﻿namespace ConsoleAppDucksAfter.Interfaces
 {
     public interface IFly
     {
